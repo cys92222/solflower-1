f2cabdccad4ef4b0e3734c1e1879eaa643a3be37
solyi <choi-solyi@naver.com>
2021-06-04 00:50:08 +0900

    cc

---
+++ src/main/java/solflower/choi/main/SolflowerApplication.java
@@ -0,0 +1,13 @@
+package solflower.choi.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolflowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolflowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/solflower/choi/main/SolflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package solflower.choi.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolflowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
