29b0a523ac05bf7eb6d81cdd7d11b6903d51560d
cys <cys92222@naver.com>
2021-06-08 12:34:30 +0900

    [2021.06.08] 타일즈 초기 설정

--- src/main/java/solflower/choi/main/board/controller/YoungsooBoardController.java
+++ src/main/java/solflower/choi/main/board/controller/YoungsooBoardController.java
@@ -2,6 +2,8 @@ package solflower.choi.main.board.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
